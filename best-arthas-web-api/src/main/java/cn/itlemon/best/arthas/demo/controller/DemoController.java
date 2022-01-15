661dc5f4a0f18a54c9aa550783ac213fc23b78e5
itlemon <lemon_jiang@aliyun.com>
2022-01-15 22:49:22 +0800

    mod: 优化protobuf

---
+++ best-arthas-web-api/src/main/java/cn/itlemon/best/arthas/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package cn.itlemon.best.arthas.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author itlemon <lemon_jiang@aliyun.com>
+ * Created on 2022-01-15
+ */
+@RestController
+public class DemoController {
+
+
+
+}
