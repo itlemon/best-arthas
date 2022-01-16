3ecfd67c6cac5aa490f5485c244d92837cb2dc41
itlemon <lemon_jiang@aliyun.com>
2022-01-16 21:52:19 +0800

    mod: 添加自定义注解，实现返回体统一包装

--- best-arthas-web-api/src/main/java/cn/itlemon/best/arthas/demo/controller/DemoController.java
+++ best-arthas-web-api/src/main/java/cn/itlemon/best/arthas/demo/controller/DemoController.java
@@ -1,7 +1,13 @@
 package cn.itlemon.best.arthas.demo.controller;
 
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import cn.itlemon.best.arthas.http.DemoResponseBody;
 import cn.itlemon.best.arthas.http.advice.ResponseResult;
@@ -19,12 +25,18 @@ public class DemoController {
         return "index";
     }
 
-    @GetMapping("/v1")
+    @GetMapping(value = "/v1", produces = "application/json;charset=utf-8")
     public String index2() {
-        return DemoResponseBody.newBuilder()
+
+        HttpServletResponse response = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getResponse();
+        assert response != null;
+        System.out.println("Default charset: " + response.getCharacterEncoding());
+
+        String s = DemoResponseBody.newBuilder()
                 .setCode("succ")
                 .setMessage("请求成功")
                 .build().toString();
+        return s;
     }
 
 
